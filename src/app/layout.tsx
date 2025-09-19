7472e7ee0a5bbc5b4a6d31159c42939bf7811d42
meheraj786 <meherajhosen786@gmail.com>
2025-09-20 02:11:52 +0600

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
+import Header1 from "../compo/Header";
+import Footer4Col from "@/compo/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -10,6 +12,13 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-poppins', 
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'] 
 });
 
 export const metadata: Metadata = {
@@ -25,9 +34,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className="dark font-poppins"
       >
+        <Header1/>
         {children}
+        <Footer4Col/>
       </body>
     </html>
   );
